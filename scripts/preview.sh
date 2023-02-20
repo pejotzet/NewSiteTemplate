ab0be9641c834ac8ef19775dcdfb2579df23067d
pzktit <Piotr.Zawadzki@polsl.pl>
2023-02-20 08:55:34 +0100

    config update

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -2,5 +2,5 @@
 
 ${BASH_SOURCE%/*}/set_env_local.sh
 bundle
-bundle exec jekyll serve --livereload --trace
+bundle exec jekyll serve --drafts --force_polling --livereload
 ${BASH_SOURCE%/*}/set_env_ghpages.sh
